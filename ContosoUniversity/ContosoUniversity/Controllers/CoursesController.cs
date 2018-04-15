5207c38647c30cadf1435b9eb74e7fba3e1446b3
hjyanghj <hjyanghj@gmail.com>
2018-04-15 22:20:22 +0800

    Update Related Data (7/10)

--- ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
@@ -38,6 +38,7 @@ namespace ContosoUniversity.Controllers
 
             var course = await _context.Courses
                 .Include(c => c.Department)
+				.AsNoTracking()
                 .SingleOrDefaultAsync(m => m.CourseID == id);
             if (course == null)
             {
@@ -47,85 +48,83 @@ namespace ContosoUniversity.Controllers
             return View(course);
         }
 
-        // GET: Courses/Create
-        public IActionResult Create()
-        {
-            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
-            return View();
-        }
+		// GET: Courses/Create
+		public IActionResult Create()
+		{
+			PopulateDepartmentsDropDownList();
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("CourseID,Credits,DepartmentID,Title")] Course course)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(course);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			PopulateDepartmentsDropDownList(course.DepartmentID);
+			return View(course);
+		}
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Courses/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CourseID,Title,Credits,DepartmentID")] Course course)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", course.DepartmentID);
-            return View(course);
-        }
+			var course = await _context.Courses
+				.AsNoTracking()
+				.SingleOrDefaultAsync(m => m.CourseID == id);
+			if (course == null)
+			{
+				return NotFound();
+			}
+			PopulateDepartmentsDropDownList(course.DepartmentID);
+			return View(course);
+		}
+		[HttpPost, ActionName("Edit")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> EditPost(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Courses/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			var courseToUpdate = await _context.Courses
+				.SingleOrDefaultAsync(c => c.CourseID == id);
 
-            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseID == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", course.DepartmentID);
-            return View(course);
-        }
-
-        // POST: Courses/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CourseID,Title,Credits,DepartmentID")] Course course)
-        {
-            if (id != course.CourseID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(course);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CourseExists(course.CourseID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", course.DepartmentID);
-            return View(course);
-        }
-
-        // GET: Courses/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+			if (await TryUpdateModelAsync<Course>(courseToUpdate,
+				"",
+				c => c.Credits, c => c.DepartmentID, c => c.Title))
+			{
+				try
+				{
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateException /* ex */)
+				{
+					//Log the error (uncomment ex variable name and write a log.)
+					ModelState.AddModelError("", "Unable to save changes. " +
+						"Try again, and if the problem persists, " +
+						"see your system administrator.");
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			PopulateDepartmentsDropDownList(courseToUpdate.DepartmentID);
+			return View(courseToUpdate);
+		}
+		private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+		{
+			var departmentsQuery = from d in _context.Departments
+								   orderby d.Name
+								   select d;
+			ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
+		}
+		// GET: Courses/Delete/5
+		public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
@@ -134,6 +133,7 @@ namespace ContosoUniversity.Controllers
 
             var course = await _context.Courses
                 .Include(c => c.Department)
+				.AsNoTracking()
                 .SingleOrDefaultAsync(m => m.CourseID == id);
             if (course == null)
             {
